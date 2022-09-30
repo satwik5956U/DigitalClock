c70f517e3f11a8750716459e5492391afe59c82e
satwik5956U <108977873+satwik5956U@users.noreply.github.com>
2022-09-30 09:19:25 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import DigitalClock from "./components/DigitalClock";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <DigitalClock />
+    </div>
+  );
+}
